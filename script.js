4d5264043a465ef2aba8845eb553fe9684a49321
Kay Jan WONG <kay.jan@hotmail.com>
2021-05-22 18:20:57 +0800

    change html js and css

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+	rate = document.getElementById("rate").value;
+	year = document.getElementById("years").value;
+	var year2 = new Date().getFullYear() + parseInt(year);
+	var value = p * (1 + rate / 100 * year) - p;
+	result = "If you deposit <mark>" + p + "</mark>,<br>at an interest rate of <mark>" + rate + "%</mark>.<br>You will receive an amount of <mark>" + value + "</mark>,<br>in the year <mark>" + year2 + "</mark>";
+	document.getElementById("result").innerHTML = result;
 }
-        
+
+function updateTextInput(value) {
+	p = document.getElementById("textInput").value;
+	p = value;
+}
