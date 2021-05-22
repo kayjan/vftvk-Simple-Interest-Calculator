f93980a78be0c7044f9be9ba67b8229e510f21d1
Kay Jan WONG <kay.jan@hotmail.com>
2021-05-22 18:33:00 +0800

    add enhancements

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+	if (p <= 0) {
+		alert("Please input a positive number");
+		document.getElementById("principal").focus();
+	}
 	rate = document.getElementById("rate").value;
 	year = document.getElementById("years").value;
 	var year2 = new Date().getFullYear() + parseInt(year);
 	var value = p * (1 + rate / 100 * year) - p;
-	result = "If you deposit <mark>" + p + "</mark>,<br>at an interest rate of <mark>" + rate + "%</mark>.<br>You will receive an amount of <mark>" + value + "</mark>,<br>in the year <mark>" + year2 + "</mark>";
+	var total = value + parseInt(p);
+	result = "If you deposit <mark>" + p + "</mark>, at an interest rate of <mark>" + rate + "%</mark>.<br>You will receive an amount of <mark>" + value + "</mark>, in the year <mark>" + year2 + "</mark>.<br>This makes the total sum <mark>" + total + "</mark>.";
 	document.getElementById("result").innerHTML = result;
 }
 
